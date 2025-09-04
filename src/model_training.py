be573b260b6a1080080cd4920a0b288b19ba1ee4
Harshita Singh Raghav <rharshita515@gmail.com>
2025-09-04 12:41:14 +0530

    changes has been commeted to the respective files.

--- src/model_training.py
+++ src/model_training.py
@@ -30,6 +30,23 @@ file_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
 
+def load_params(params_path: str) -> dict:
+    """Load parameters from a YAML file."""
+    try:
+        with open(params_path, 'r') as file:
+            params = yaml.safe_load(file)
+        logger.debug('Parameters retrieved from %s', params_path)
+        return params
+    except FileNotFoundError:
+        logger.error('File not found: %s', params_path)
+        raise
+    except yaml.YAMLError as e:
+        logger.error('YAML error: %s', e)
+        raise
+    except Exception as e:
+        logger.error('Unexpected error: %s', e)
+        raise
+
 def load_data(file_path:str)->pd.DataFrame:
     """
     Load data from a CSV file.
@@ -100,7 +117,9 @@ def save_model(model:RandomForestClassifier,model_path:str)->None:
 
 def main():
     try:
-        params={'n_estimators':25,'random_state':42}
+        params = load_params(params_path='params.yaml')['model_training']
+
+        # params={'n_estimators':25,'random_state':42}
         train_data=load_data('./data/featured/train_tfidf.csv')
         x_train=train_data.drop(columns=['label']).values
         y_train=train_data['label'].values
